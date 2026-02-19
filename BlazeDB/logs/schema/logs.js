39420a47b9f62add651459c2e00ac2a2b35ec8f6
Voyrox <ewen@macculloch.net>
2026-02-19 20:07:50 +1100

    V1

--- BlazeDB/logs/schema/logs.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-module.exports = mongoose.model("Logs", new mongoose.Schema({
-    token: { type: String, default: null },
-    dbname: { type: String, default: null },
-    logs: { type: Array, default: [] },
-    registeredAt: { type: Number, default: Date.now() },
- }));
